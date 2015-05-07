2d5f6c1ac76eeb7e80fb0b5f8486445882e6929f
EdPassos <ed.passos@gmail.com>
2015-05-07 23:19:05 +0100

    Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/welcome'
+
   devise_for :users
   resources :people
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'people#index'
+root 'pages#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
