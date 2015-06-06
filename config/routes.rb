232a6ee38a7529820d7462850b82b11666307810
EdPassos <ed.passos@gmail.com>
2015-06-06 12:54:12 +0100

    user can have personal details

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users do 
-	resources :people
+  resources :users do
   end
+
+	resources :people
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'pages#welcome'
+	root 'pages#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504201013) do
+ActiveRecord::Schema.define(version: 20150606114739) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name",             limit: 255
@@ -25,11 +25,13 @@ ActiveRecord::Schema.define(version: 20150504201013) do
     t.integer  "socialsecurity",   limit: 4
     t.integer  "idnumber",         limit: 4
     t.integer  "phonenumber",      limit: 4
-    t.string   "email",            limit: 255
     t.string   "maritalstatus",    limit: 255
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.integer  "user_id",          limit: 4
   end
+
+  add_index "people", ["user_id"], name: "index_people_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -49,4 +51,5 @@ ActiveRecord::Schema.define(version: 20150504201013) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "people", "users"
 end
